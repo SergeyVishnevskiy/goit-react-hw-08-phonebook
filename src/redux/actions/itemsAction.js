c2974be5c437ebd0fa324a309b27522b95d760e7
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-09 17:12:05 +0200

    add

--- src/redux/actions/itemsAction.js
+++ src/redux/actions/itemsAction.js
@@ -6,7 +6,7 @@ import {
 } from "../constans/constants";
 import { createAction } from "@reduxjs/toolkit";
 
-export const userGet = createAction(GET_USERS);
+export const usersGet = createAction(GET_USERS);
 export const userAdd = createAction(ADD_USER);
 export const userDelete = createAction(DELETE_USER);
 export const userClear = createAction(CLEAR_USER);
