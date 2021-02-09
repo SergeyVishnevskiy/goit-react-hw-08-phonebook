c5d360e044805153cee2cd0c9ee173d3f7faa595
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-09 17:12:32 +0200

    add

--- src/redux/reducers/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import phonebookReducer from "./reducer";
-
-const rootReducer = combineReducers({
-  contacts: phonebookReducer,
-});
-export default rootReducer;
