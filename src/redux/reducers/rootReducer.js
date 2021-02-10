cdb6c158ace6754edce22b7513a697c0c9659e19
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-10 19:19:49 +0200

    add

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,7 +1,18 @@
 import { combineReducers } from "redux";
-import phonebookReducer from "./reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import phonebookReducer from "./phoneReducer";
+import authReducer from "./authReducer";
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
 
 const rootReducer = combineReducers({
   contacts: phonebookReducer,
+  auth: persistReducer(authPersistConfig, authReducer),
 });
+
 export default rootReducer;
