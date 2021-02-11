4554ca2098fbd8bfc21273dc4d355685420db427
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-11 12:47:34 +0200

    add

--- src/components/Header/navigation/NavigationLink.js
+++ src/components/Header/navigation/NavigationLink.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
 
 const NavigationLink = ({ path, exact, name }) => (
   <li>
     <NavLink
       to={path}
       exact={exact}
-      className="navagationLink"
-      activeClassName="activeLink"
+      className={styles.navLink}
+      activeClassName={styles.activeLink}
     >
       {name}
     </NavLink>
