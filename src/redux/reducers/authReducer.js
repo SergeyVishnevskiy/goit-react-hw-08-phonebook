1d165b4086fef851861ff438590b6061803a8206
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-11 12:47:52 +0200

    add

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -6,28 +6,26 @@ import {
   loginError,
   logoutSuccess,
   logoutError,
-  getCurrentUserSuccess,
-  getCurrentUserError,
 } from "../actions/authAction";
 
 const initUserState = { user: null, email: null };
 
 const user = createReducer(initUserState, {
-  [registerSuccess]: (_, { payload }) => payload.token,
-  [loginSuccess]: (_, { payload }) => payload.user,
-  [getCurrentUserSuccess]: (_, { payload }) => payload,
+  [registerSuccess]: (state, { payload }) => ({ ...state, payload }),
+  [loginSuccess]: (state, { payload }) => ({ ...state, payload }),
+  // [getCurrentUserSuccess]: (_, { payload }) => payload,
   [logoutSuccess]: () => initUserState,
 });
 const token = createReducer(null, {
-  [registerSuccess]: (_, { payload }) => payload.token,
-  [loginSuccess]: (_, { payload }) => payload.token,
+  [registerSuccess]: (_, { payload }) => payload.idToken,
+  [loginSuccess]: (_, { payload }) => payload.idToken,
   [logoutSuccess]: () => null,
 });
 const error = createReducer(null, {
   [registerError]: (_, { payload }) => payload,
   [loginError]: (_, { payload }) => payload,
   [logoutError]: (_, { payload }) => payload,
-  [getCurrentUserError]: (_, { payload }) => payload,
+  // [getCurrentUserError]: (_, { payload }) => payload,
 });
 
 const authReducer = combineReducers({
